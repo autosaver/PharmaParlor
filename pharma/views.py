e2beece59a4a1cc94806b45ca974099066fbde0f
Brian Otieno <brianokola@gmail.com>
2020-07-18 06:10:17 +0300

    Add Featured Products

--- pharma/views.py
+++ pharma/views.py
@@ -4,6 +4,15 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+import random
+from .forms import *
+from .models import *
 
 # Create your views here.
 def home(request):
